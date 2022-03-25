4f9164b53c6dd8b46760dcd1095fe6a5be7e762f
idzharism <idzharism@student.telkomuniversity.ac.id>
2022-03-25 16:07:48 +0700

    implementasi design by contract

--- tpmodul5_1302190047/Program.cs
+++ tpmodul5_1302190047/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul5_1302190047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo saya = new SayaTubeVideo("Tutorial Design By Contract - Idzhari Syaeful M");
+            saya.IncreasePlayCount(0);
+            saya.PrintVideoDetails();
+
+            Console.WriteLine();
+
+            SayaTubeVideo saya1 = new SayaTubeVideo("Idzhari Syaeful M");
+            saya1.IncreasePlayCount(1000000000);
+            saya1.PrintVideoDetails();
+        }
+    }
+}
